8de26333d7631495925946e55684236bde6aee0f
ammanueladdis <ammanueladdis@gmail.com>
2018-11-06 13:58:55 +1300

    Page 77 - making a call to an external Class and passing an argument

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,9 +1,9 @@
-// Fig. 3.1: GradeBook.java
-// Class declaration with one method.
+// Fig. 3.4: GradeBook.java
+// Class declaration with one method that has a parameter
 
 public class GradeBook {
     // display a welcome message to the GradeBook user
-    public void displayMessage() {
-        System.out.println("Welcome to the Grade Book!");
+    public void displayMessage(String courseName) {
+        System.out.printf("Welcome to the Grade Book for\n%s!\n", courseName);
     }
 }
--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -1,11 +1,19 @@
-// Fig. 3.2: GradeBookTest.java
-// Creating a GradeBook object and calling its displayMessage() method.
+// Fig. 3.5: GradeBookTest.java
+// Creating a GradeBook object and pass a String to its displayMessage method
+import java.util.Scanner;
 
 public class GradeBookTest {
     public static void main(String args[]) {
+        // create a Scanner to obtain input from the command window
+        Scanner input = new Scanner(System.in);
         // create a GradeBook object and assign it to myGradeBook
         GradeBook myGradeBook = new GradeBook();
-        // call the displayMessage method from myGradeBook
-        myGradeBook.displayMessage();
+        //prompt for and input the course name
+        System.out.println("Please enter the course name:");
+        String nameOfCourse = input.nextLine(); // read the line of text
+        System.out.println(); // output a blank line
+
+        // call the displayMessage method from myGradeBook and pass an argument called nameOfCourse
+        myGradeBook.displayMessage(nameOfCourse);
     }
 }
