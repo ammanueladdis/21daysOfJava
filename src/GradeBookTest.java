c4180cb3d0206dc8b53eeb7ab348a6504d97d94a
ammanueladdis <ammanueladdis@gmail.com>
2018-11-06 18:35:30 +1300

    Fig 3.7 - done for the dayyyy

--- src/GradeBookTest.java
+++ src/GradeBookTest.java
@@ -1,5 +1,5 @@
-// Fig. 3.5: GradeBookTest.java
-// Creating a GradeBook object and pass a String to its displayMessage method
+// Fig. 3.8: GradeBookTest.java
+// Creating and manipulating a GradeBook object.
 import java.util.Scanner;
 
 public class GradeBookTest {
@@ -8,12 +8,14 @@ public class GradeBookTest {
         Scanner input = new Scanner(System.in);
         // create a GradeBook object and assign it to myGradeBook
         GradeBook myGradeBook = new GradeBook();
-        //prompt for and input the course name
+        // display initial value of courseName
+        System.out.printf("Initial course name is %s\n\n", myGradeBook.getCourseName());
+        // prompt for and read the course name
         System.out.println("Please enter the course name:");
-        String nameOfCourse = input.nextLine(); // read the line of text
+        String theName = input.nextLine(); // read a line of text
+        myGradeBook.setCourseName(theName); // set the course name from the input
         System.out.println(); // output a blank line
-
         // call the displayMessage method from myGradeBook and pass an argument called nameOfCourse
-        myGradeBook.displayMessage(nameOfCourse);
+        myGradeBook.displayMessage();
     }
 }
