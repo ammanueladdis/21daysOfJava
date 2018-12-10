3ce8b57a5219ddc04e2ba38d33fcbe6c8fd1b938
manny <ammanueladdis@gmail.com>
2018-12-10 18:33:07 +1300

    back in it 7.4 InitArray.java

--- src/InitArray.java
+++ src/InitArray.java
@@ -1,16 +1,32 @@
-// Fig. 7.3: InitArray.java
-// Initializing the elements of an array to default values of zero.
+// Fig. 7.4: InitArray.java
+// Calculating the values to be placed into the elements of an array.
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
 
 public class InitArray {
     public static void main(String[] args) {
-        // initializer list specifies the initial value for each element
-        int[] array = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37 };
+        final int ARRAY_LENGTH = 10; // declare a new constant
+        int[] array = new int[ARRAY_LENGTH]; // create the array
 
-        System.out.printf("%s%8s%n","Index", "Value"); // column headings
-
-        // output each array element's value
+        // calculate the value for each array element
         for (int counter = 0; counter < array.length; counter++)
-            System.out.printf("%5d%8d%n", counter, array[counter]);
+            array[counter] = 2 + 2 * counter;
 
+        System.out.printf("%s%16s%n", "Index", "Value"); // print out the column headings
+
+        // output each
+        for (int i = 0; i < array.length; i++)
+            System.out.printf("%5d%16d%n", i, array[i]);
+
+        // Manny fucking around and testing a date to string API
+        // declare new date variable date
+        Date date = new Date();
+        // initialize the SimpleDateFormat class and set the pattern
+        DateFormat dateFormat = new SimpleDateFormat("dd-MMMM-yyyy");
+        //initialize variable and use dateFormat to convert date to string
+        String strDate = dateFormat.format(date);
+        System.out.println("The current time is " + strDate + ".");
     }
 }
